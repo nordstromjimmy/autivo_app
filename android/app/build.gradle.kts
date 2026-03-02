1ab1dd681fdd8a393646fb140898a7fdbe966697
nordstromjimmy <nordstromjimmy@gmail.com>
2026-03-02 18:53:53 +0100

    update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.mina_fordon"
-    compileSdk = 36 
+    compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -24,11 +24,10 @@ android {
         applicationId = "com.example.mina_fordon"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 24
-        targetSdk = 36  
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-        multiDexEnabled = true
     }
 
     buildTypes {
@@ -36,36 +35,13 @@ android {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
-            
-            // NEW: Disable minify to avoid R8 errors with ML Kit
+
             isMinifyEnabled = false
             isShrinkResources = false
-        }
-    }
-    
-    // NEW: Packaging options to avoid conflicts
-    packaging {
-        resources {
-            excludes += listOf(
-                "META-INF/DEPENDENCIES",
-                "META-INF/LICENSE",
-                "META-INF/LICENSE.txt",
-                "META-INF/license.txt",
-                "META-INF/NOTICE",
-                "META-INF/NOTICE.txt",
-                "META-INF/notice.txt",
-                "META-INF/ASL2.0",
-                "META-INF/*.kotlin_module"
-            )
         }
     }
 }
 
 flutter {
     source = "../.."
-}
-
-// NEW: Add MultiDex dependency
-dependencies {
-    implementation("androidx.multidex:multidex:2.0.1")
 }
